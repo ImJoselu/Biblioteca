e6663d43926dcac0d88af8f153932bc5fe629b92
CicloM <CicloM@DESKTOP-QCCISJO>
2023-02-09 10:49:06 +0100

    Commit SolictudDTO

--- biblioteca/src/main/java/com/example/demo/web/controller/SolicitudController.java
+++ biblioteca/src/main/java/com/example/demo/web/controller/SolicitudController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.example.demo.model.dto.ClienteDTO;
+
 import com.example.demo.model.dto.EjemplarDTO;
 import com.example.demo.model.dto.SolicitudDTO;
 import com.example.demo.model.dto.UsuarioDTO;
